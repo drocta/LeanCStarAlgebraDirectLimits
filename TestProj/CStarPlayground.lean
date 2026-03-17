5b3c0cd0d9888856a5db7243171f950cab8663f5
drocta <madaco.madaco@gmail.com>
2026-03-17 14:03:19 -0700

    add lemmas amplify_apply and amplifyAssoc

--- TestProj/CStarPlayground.lean
+++ TestProj/CStarPlayground.lean
@@ -3,6 +3,7 @@ import Mathlib.Analysis.CStarAlgebra.CStarMatrix
 import Mathlib.Analysis.CStarAlgebra.Matrix
 import Mathlib.LinearAlgebra.Matrix.Kronecker
 import Mathlib.LinearAlgebra.Matrix.Defs
+import Mathlib.LinearAlgebra.Matrix.Reindex
 import Mathlib.Algebra.Star.StarAlgHom
 import Mathlib.RingTheory.MatrixAlgebra
 import Mathlib.RingTheory.TensorProduct.Basic
@@ -184,6 +185,7 @@ example [Fintype m] [Fintype n] [NonUnitalNormedRing ℂ] [StarRing ℂ]
     exact h
 
 variable (mk : Type*) [Fintype mk] [DecidableEq mk]
+variable (mk2 : Type*) [Fintype mk2] [DecidableEq mk2]
 
 #check (inferInstance : Inhabited Nat)
 
@@ -201,7 +203,7 @@ def raw_amplify (n k : Type*) [Fintype n] [Fintype k] [DecidableEq k]
     (A : Matrix n n ℂ) : Matrix (n × k) (n × k) ℂ :=
     A ⊗ₖ (1 : Matrix k k ℂ)
 
-noncomputable def amplify
+noncomputable def amplify0
     (n k : Type*)
     [Fintype n] [DecidableEq n]
     [Fintype k] [DecidableEq k] :
@@ -280,7 +282,8 @@ noncomputable def amplify2
     [Fintype n] [DecidableEq n]
     [Fintype k] [DecidableEq k] :
     Matrix n n ℂ →⋆ₐ[ℂ] Matrix (n × k) (n × k) ℂ :=
-  ((Matrix.kroneckerStarAlgEquiv n k ℂ): StarAlgHom ℂ ((Matrix n n ℂ) ⊗[ℂ] (Matrix k k ℂ)) (Matrix (n × k) (n × k) ℂ)).comp
+  ((Matrix.kroneckerStarAlgEquiv n k ℂ):
+  StarAlgHom ℂ ((Matrix n n ℂ) ⊗[ℂ] (Matrix k k ℂ)) (Matrix (n × k) (n × k) ℂ)).comp
     (StarAlgHom.ofAlgHom
       (Algebra.TensorProduct.includeLeft : Matrix n n ℂ →ₐ[ℂ]
         TensorProduct ℂ (Matrix n n ℂ) (Matrix k k ℂ)))
@@ -301,7 +304,7 @@ noncomputable def amplify3
 
 
 
-
+-- set_option trace.Meta.synthInstance true in
 noncomputable def amplify4
     (n k : Type*)
     [Fintype n] [DecidableEq n]
@@ -342,7 +345,7 @@ open scoped TensorProduct
 
 
 
-#check Matrix mn mn ℂ →ₐ[ℂ] Matrix mn mn ℂ ⊗[ℂ] Matrix mk mk ℂ
+-- #check Matrix mn mn ℂ →ₐ[ℂ] Matrix mn mn ℂ ⊗[ℂ] Matrix mk mk ℂ
 
 noncomputable def amplify7
     (n k : Type*)
@@ -361,7 +364,7 @@ noncomputable def amplify7
 }
 
 
-noncomputable def amplify8
+noncomputable def amplify
     (n k : Type*)
     [Fintype n] [DecidableEq n]
     [Fintype k] [DecidableEq k] :
@@ -372,14 +375,131 @@ noncomputable def amplify8
         Algebra.TensorProduct.instAlgebra
     exact
 {
-map_star' := by
-    intro A
-    simpa using
-    (Matrix.kroneckerStarAlgEquiv n k ℂ).map_star' (A ⊗ₜ[ℂ] (1 : Matrix k k ℂ))
 toAlgHom :=
     (Matrix.kroneckerStarAlgEquiv n k ℂ).toAlgEquiv.toAlgHom.comp
     (Algebra.TensorProduct.includeLeft :
         Matrix n n ℂ →ₐ[ℂ] Matrix n n ℂ ⊗[ℂ] Matrix k k ℂ)
+map_star' := by
+    intro A
+    simpa using
+    (Matrix.kroneckerStarAlgEquiv n k ℂ).map_star' (A ⊗ₜ[ℂ] (1 : Matrix k k ℂ))
 }
 
-#check amplify8
+#check amplify
+
+@[simp] lemma amplify_apply
+    (n k : Type*) [Fintype n] [DecidableEq n] [Fintype k] [DecidableEq k]
+    (A : Matrix n n ℂ) :
+    amplify n k A = A ⊗ₖ (1 : Matrix k k ℂ) := by
+  simp only [amplify, toAlgEquiv_kroneckerStarAlgEquiv, AlgEquiv.toAlgHom_eq_coe,
+    StarAlgHom.coe_mk', AlgHom.coe_comp, AlgHom.coe_coe, Function.comp_apply,
+    kroneckerAlgEquiv_apply]
+  have h :
+    (Algebra.TensorProduct.includeLeft (R := ℂ) (S := ℂ)
+      (A := Matrix n n ℂ) (B := Matrix k k ℂ) A)
+    =
+    A ⊗ₜ[ℂ] (1 : Matrix k k ℂ) :=
+        (Algebra.TensorProduct.includeLeft_apply
+          (R := ℂ) (S := ℂ)
+          (A := Matrix n n ℂ) (B := Matrix k k ℂ) A)
+  change
+    kroneckerLinearEquiv n n k k ℂ
+      (A ⊗ₜ[ℂ] (1 : Matrix k k ℂ))
+      =
+    A ⊗ₖ (1 : Matrix k k ℂ)
+  rw [← h]
+  exact
+    (kroneckerLinearEquiv_tmul
+    (l:= n) (m := n) (n := k) (p := k)
+    (R := ℂ)
+    A (1 : Matrix k k ℂ))
+   -- simp [amplify, Algebra.TensorProduct.includeLeft_apply]
+  -- rw [Algebra.TensorProduct.includeLeft_apply A]
+
+
+
+noncomputable def amplify9
+    (n k : Type*)
+    [Fintype n] [DecidableEq n]
+    [Fintype k] [DecidableEq k] :
+    Matrix n n ℂ →⋆ₐ[ℂ] Matrix (n × k) (n × k) ℂ := by
+    letI : Semiring ((Matrix n n ℂ) ⊗[ℂ] (Matrix k k ℂ)) :=
+        @Algebra.TensorProduct.instSemiring ℂ (Matrix n n ℂ) (Matrix k k ℂ) _ _ _ _ _
+    letI : Algebra ℂ ((Matrix n n ℂ) ⊗[ℂ] (Matrix k k ℂ)) :=
+        Algebra.TensorProduct.instAlgebra
+    let amplifyAlgHom : Matrix n n ℂ →ₐ[ℂ] Matrix (n × k) (n × k) ℂ :=
+        (Matrix.kroneckerStarAlgEquiv n k ℂ).toAlgEquiv.toAlgHom.comp
+        (Algebra.TensorProduct.includeLeft :
+            Matrix n n ℂ →ₐ[ℂ] Matrix n n ℂ ⊗[ℂ] Matrix k k ℂ)
+    exact
+{
+toAlgHom := amplifyAlgHom
+map_star' := by
+    intro A
+    simpa using
+    (Matrix.kroneckerStarAlgEquiv n k ℂ).map_star' (A ⊗ₜ[ℂ] (1 : Matrix k k ℂ))
+}
+
+#check amplify9
+
+
+#check Equiv.prodAssoc mn mk mk2
+#check Matrix.reindexAlgEquiv ℂ ℂ (Equiv.prodAssoc mn mk mk2)
+
+
+#check (amplify4 (mn × mk) mk2).comp (amplify4 mn mk)
+#check (amplify4 mn mk)
+#check (amplify4 mn (mk × mk2)).toAlgHom
+#check (Matrix.reindexAlgEquiv ℂ ℂ (Equiv.prodAssoc mn mk mk2)).toAlgHom
+
+
+noncomputable def foo1 := ((amplify (mn × mk) mk2).comp (amplify mn mk)).toAlgHom
+#check foo1
+
+noncomputable def foo2 := (Matrix.reindexAlgEquiv ℂ ℂ (Equiv.prodAssoc mn mk mk2)).toAlgHom
+#check foo2
+
+noncomputable def foo3 := (foo2 mk mk2 mn).comp (foo1 mk mk2 mn)
+
+#check foo3
+
+noncomputable def foo4 := ((Matrix.reindexAlgEquiv ℂ ℂ (Equiv.prodAssoc mn mk mk2)).toAlgHom).comp
+    (((amplify (mn × mk) mk2).comp (amplify mn mk)).toAlgHom)
+
+#check foo4
+
+noncomputable def foo5 := (amplify4 mn (mk × mk2)).toAlgHom
+#check foo5
+
+/-
+have hkron1 : (1 : Matrix k k ℂ) ⊗ₖ (1 : Matrix k2 k2 ℂ) = (1 : Matrix (k × k2) (k × k2) ℂ)
+    := one_kronecker_one
+-/
+
+lemma amplifyAssoc
+    (n k k2 : Type*) [Fintype n] [DecidableEq n] [Fintype k] [DecidableEq k]
+    [Fintype k2] [DecidableEq k2] :
+    ((Matrix.reindexAlgEquiv ℂ ℂ (Equiv.prodAssoc n k k2)).toAlgHom).comp
+      (((amplify (n × k) k2).comp (amplify n k)).toAlgHom) =
+    (amplify n (k × k2)).toAlgHom := by
+    ext A i j
+    rcases i with ⟨i₁, i₂, i₃⟩
+    rcases j with ⟨j₁, j₂, j₃⟩
+    simp only [AlgEquiv.toAlgHom_eq_coe, AlgHom.coe_comp, AlgHom.coe_coe, StarAlgHom.coe_toAlgHom,
+      StarAlgHom.coe_comp, Function.comp_apply, amplify_apply, reindexAlgEquiv_apply, reindex_apply,
+      submatrix_apply, Equiv.prodAssoc_symm_apply, kroneckerMap_apply]
+    rw [one_apply, one_apply, one_apply, mul_boole, mul_boole, mul_boole]
+    simp only [Prod.mk.injEq]
+    rw [← ite_and]
+    simp only [and_comm]
+
+/-
+    rw?
+    simp only [hkron1, AlgEquiv.toAlgHom_eq_coe, AlgHom.coe_comp, AlgHom.coe_coe, StarAlgHom.coe_toAlgHom,
+      StarAlgHom.coe_comp, Function.comp_apply, reindexAlgEquiv_apply, reindex_apply,
+      submatrix_apply, Equiv.prodAssoc_symm_apply]
+    rw?
+    simp only [AlgHom.comp_apply, Matrix.reindexAlgEquiv_apply, Equiv.prodAssoc_apply,
+      amplify, StarAlgHom.comp_apply, StarAlgHom.toAlgHom_apply]
+
+-/
