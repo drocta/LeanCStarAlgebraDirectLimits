20d76a996a5af4e8ae7d3f6098313002f971e72f
drocta <madaco.madaco@gmail.com>
2026-03-23 17:04:56 -0700

    wip stuff with cocones over directed systems of c star algebras, and a bit with the limiting thing.

--- TestProj/CStarPlayground.lean
+++ TestProj/CStarPlayground.lean
@@ -7,6 +7,8 @@ import Mathlib.LinearAlgebra.Matrix.Reindex
 import Mathlib.Algebra.Star.StarAlgHom
 import Mathlib.RingTheory.MatrixAlgebra
 import Mathlib.RingTheory.TensorProduct.Basic
+import Mathlib.Order.DirectedInverseSystem
+import Mathlib.Order.SetNotation
 
 #check SeminormedAddCommGroup
 #check StarRing
@@ -245,6 +247,8 @@ noncomputable def amplify0
 open scoped TensorProduct
 
 variable (mn : Type*) [Fintype mn] [DecidableEq mn]
+
+#check mn
 
 #synth Semiring (Matrix mn mn ℂ)
 #synth Algebra ℂ (Matrix mn mn ℂ)
@@ -492,3 +496,162 @@ lemma amplify_assoc
       amplify, StarAlgHom.comp_apply, StarAlgHom.toAlgHom_apply]
 
 -/
+
+lemma amplify_assoc2
+  (n k k2 : Type*) [Fintype n] [DecidableEq n] [Fintype k] [DecidableEq k]
+  [Fintype k2] [DecidableEq k2] :
+  ((Matrix.reindexAlgEquiv ℂ ℂ (Equiv.prodAssoc n k k2)).toAlgHom).comp
+    (((amplify (n × k) k2).comp (amplify n k)).toAlgHom) =
+  (amplify n (k × k2)).toAlgHom := by
+  ext A
+  have h := Matrix.kronecker_assoc A (1 : Matrix k k ℂ) (1 : Matrix k2 k2 ℂ)
+  simp [amplify_apply, h]
+
+
+namespace Matrix
+
+/-- For square matrices with coefficients in a star algebra over a commutative semiring, the natural
+map that reindexes a matrix's rows and columns with equivalent types,
+`Matrix.reindex`, is an equivalence of star algebras. -/
+def reindexStarAlgEquiv {n m R A : Type*} [CommSemiring R]
+    [Fintype n] [DecidableEq n] [Fintype m] [DecidableEq m]
+    [Star A] [Semiring A] [Algebra R A] (e : m ≃ n) :
+    Matrix m m A ≃⋆ₐ[R] Matrix n n A :=
+  {(Matrix.reindexAlgEquiv R A e).toRingEquiv with
+    toFun := reindex e e
+    map_smul' := by
+      intro r M
+      ext i j
+      rfl
+    map_star' := by
+      intro M
+      ext i j
+      rfl
+  }
+
+end Matrix
+
+
+variable {mn mm mk mk2 mR mA : Type*} [CommSemiring mR] [Semiring mA] [Star mA] [Algebra mR mA]
+--    [Fintype mn] [DecidableEq mn] [Fintype mm] [DecidableEq mm]
+--    [Fintype mk] [DecidableEq mk] [Fintype mk2] [DecidableEq mk2]
+
+example {A1 A2 : Type*} [Semiring A1] [Semiring A2] [Star A1] [Star A2]
+  [Algebra mR A1] [Algebra mR A2]
+    (eq : A1 ≃⋆ₐ[mR] A2) : A1 →⋆ₐ[mR] A2 :=
+    {
+      toAlgHom := eq.toAlgEquiv.toAlgHom
+      map_star' := eq.toStarRingEquiv.map_star'
+    }
+
+
+example {R A B : Type*} [CommSemiring R] [Semiring A] [Semiring B]
+    [Star A] [Star B] [Algebra R A] [Algebra R B]
+    (e : A ≃⋆ₐ[R] B) : A →⋆ₐ[R] B :=
+  (e : A →⋆ₐ[R] B)
+
+def StarAlgEquiv.toStarAlgHom {R A B : Type*} [CommSemiring R] [Semiring A] [Semiring B]
+    [Star A] [Star B] [Algebra R A] [Algebra R B]
+    (e : A ≃⋆ₐ[R] B) : A →⋆ₐ[R] B :=
+  (e : A →⋆ₐ[R] B)
+
+#check Matrix.reindexStarAlgEquiv
+
+#check DirectedSystem
+
+
+variable {ι : Type*} [Preorder ι]
+
+structure CoConeOfDirSysOfSTarAlg (A : Type*) (F : ι → Type*) (f : ∀ ⦃i j⦄, i ≤ j → F i → F j) extends
+    CStarAlgebra A, DirectedSystem F f where
+  obj_is_cstar_algebra i : CStarAlgebra (F i)
+  banana (i j : ι) (h : i ≤ j) : (F i) →⋆ₐ[ℂ] (F j)
+  apple {i j : ι} (h : i ≤ j) : (f h) = (banana i j h).toFun
+  inclusion i : (F i) →⋆ₐ[ℂ] A
+  commutes_inclusion {i j : ι} (h : i ≤ j) (x : F i) :
+    (inclusion j).comp (banana i j h) = inclusion i
+
+structure CStarAlgDirSysCocone
+    (F : ι → Type*) [∀ i, CStarAlgebra (F i)]
+    (F_hom : ∀ ⦃i j⦄, i ≤ j → (F i) →⋆ₐ[ℂ] (F j))
+    (A : Type*) [CStarAlgebra A] where
+  incl i : (F i) →⋆ₐ[ℂ] A
+  map_self_hom {i} : F_hom (le_refl i) = StarAlgHom.id ℂ (F i)
+  map_map_hom {k j i} (hij : i ≤ j) (hjk : j ≤ k) :
+    (( F_hom hjk ).comp (F_hom hij ) : (F i) →⋆ₐ[ℂ] (F k)) = F_hom (hij.trans hjk)
+  incl_commute {i j} (hij : i ≤ j) : (incl j).comp (F_hom hij) = incl i
+
+def CStarAlgDirSysCocone.underlyingMap
+    {F : ι → Type*} [∀ i, CStarAlgebra (F i)]
+    {A : Type*} [CStarAlgebra A]
+    {map : ∀ ⦃i j⦄, i ≤ j → (F i) →⋆ₐ[ℂ] (F j)}
+    (S : CStarAlgDirSysCocone F map A) :
+    DirectedSystem F (fun _ _ hij => (map hij).toFun) :=
+{
+  map_self ⦃i⦄ (x : F i) := by
+    simp [S.map_self_hom]
+  map_map ⦃k j i⦄ (hij : i ≤ j) (hjk : j ≤ k) := by
+    rw [@AlgHom.toFun_eq_coe]
+    simp only [AlgHom.toRingHom_eq_coe, RingHom.toMonoidHom_eq_coe, AlgHom.toRingHom_toMonoidHom,
+      OneHom.toFun_eq_coe, MonoidHom.toOneHom_coe, MonoidHom.coe_coe, StarAlgHom.coe_toAlgHom]
+    intro x
+    rw [← @StarAlgHom.comp_apply]
+    rw [S.map_map_hom]
+}
+
+structure CStarAlgDirectedSystem
+    (ι : Type*) [Preorder ι] where
+  F : ι → Type*
+  inst : ∀ i, CStarAlgebra (F i)
+  map ⦃i j⦄ (h : i ≤ j) : (F i) →⋆ₐ[ℂ] (F j)
+  map_self : ∀ ⦃i⦄, map (le_rfl : i ≤ i) = StarAlgHom.id ℂ (F i)
+  map_map : ∀ ⦃k j i⦄ (hij : i ≤ j) (hjk : j ≤ k),
+    ((map hjk).comp (map hij) : (F i) →⋆ₐ[ℂ] (F k)) = map (hij.trans hjk)
+
+attribute [instance] CStarAlgDirectedSystem.inst
+
+structure CStarAlgDirectedSystem2
+    (ι : Type*) [Preorder ι] where
+  F : ι → Type*
+  inst i : CStarAlgebra (F i)
+  map ⦃i j⦄ (hij : i ≤ j) : (F i) →⋆ₐ[ℂ] (F j)
+  map_self {i} : map (le_rfl : i ≤ i) = StarAlgHom.id ℂ (F i)
+  map_map  ⦃i j k⦄ (hij : i ≤ j) (hjk : j ≤ k) :
+    ((map hjk).comp (map hij) : (F i) →⋆ₐ[ℂ] (F k)) = map (hij.trans hjk)
+
+attribute [instance] CStarAlgDirectedSystem2.inst
+
+-- This is a version that uses the `DirectedSystem` typeclass for the coherence conditions. Not sure if it's better or worse than the previous ones.
+structure CStarSystem (ι : Type*) [Preorder ι] where
+  F : ι → Type*
+  inst : ∀ i, CStarAlgebra (F i)
+  map : ⦃i j : ι⦄ → i ≤ j → F i →⋆ₐ[ℂ] F j
+  directed : DirectedSystem F (fun _ _ h => map h)
+
+structure CStarAlgCocone0
+    (S : CStarAlgDirectedSystem2 ι)
+    (A : Type*) [CStarAlgebra A] where
+  inclusion i :  S.F i →⋆ₐ[ℂ] A
+  commute : ∀ ⦃i j⦄ (hij : i ≤ j) ,
+    (inclusion j ).comp ((S.map hij) :  (S.F i) →⋆ₐ[ℂ] (S.F j)) = inclusion i
+
+
+structure CStarAlgCocone
+    {ι : Type*} [Preorder ι]
+    (S : CStarAlgDirectedSystem ι) where
+  A : Type*
+  instA : CStarAlgebra A
+  inclusion (i : ι) : S.F i →⋆ₐ[ℂ] A
+  commute : ∀ ⦃i j⦄ (hij : i ≤ j) ,
+    (inclusion j ).comp ((S.map hij) :  (S.F i) →⋆ₐ[ℂ] (S.F j)) = inclusion i
+
+attribute [instance] CStarAlgCocone.instA
+
+def coneClosed (S : CStarAlgDirectedSystem ι) (C : CStarAlgCocone S) : Prop :=
+    closure (Set.iUnion (fun i => (Set.range (C.inclusion i)))) = Set.univ
+
+structure DenseCStarAlgCocone
+  {ι : Type*} [Preorder ι]
+  (S : CStarAlgDirectedSystem ι) extends CStarAlgCocone S where
+    --isClosureOfUnion : closure (Set.iUnion (fun i => (Set.range (inclusion i)))) = Set.univ
+    dense_iUnion_ranges : Dense (Set.iUnion (fun i => (Set.range (inclusion i))))
