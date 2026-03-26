e9128ea1e48adfd3c55b1cbc643698af8b80ddea
drocta <madaco.madaco@gmail.com>
2026-03-26 10:34:44 -0700

    remove unnecessary early declaration of implicit variable R

--- TestProj/StarDirectLimit.lean
+++ TestProj/StarDirectLimit.lean
@@ -3,7 +3,7 @@ import Mathlib.Algebra.Star.Basic
 
 namespace DirectLimit
 
-variable {R ι : Type*} [Preorder ι] {G : ι → Type*}
+variable {ι : Type*} [Preorder ι] {G : ι → Type*}
 variable {T : ∀ ⦃i j : ι⦄, i ≤ j → Type*} {f : ∀ _ _ h, T h}
 variable [∀ i j (h : i ≤ j), FunLike (T h) (G i) (G j)] [DirectedSystem G (f · · ·)]
 variable [IsDirectedOrder ι]
