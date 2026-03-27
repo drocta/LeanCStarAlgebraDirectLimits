cd75c8dda3a7afc958f813215067083f1a872bbf
drocta <madaco.madaco@gmail.com>
2026-03-26 23:51:21 -0700

    add definition for DirectLimit.Algebra.lift , for producing algebra maps out of the direct limit

--- TestProj/StarDirectLimit.lean
+++ TestProj/StarDirectLimit.lean
@@ -260,6 +260,58 @@ noncomputable def of (i : ι) : G i →ₐ[R] DirectLimit G f :=
 
 }
 
+variable (A : Type*) [Semiring A] [Algebra R A]
+
+variable (G f) in
+/-- The universal property of the direct limit: maps from the components to another R-algebra
+that respect the directed system structure (i.e. make some diagram commute) give rise
+to a unique map out of the direct limit.
+-/
+noncomputable def lift (g : ∀ i, G i →ₐ[R] A) (Hg : ∀ i j hij x, g j (f i j hij x) = g i x) :
+    DirectLimit G f →ₐ[R] A :=
+{(DirectLimit.Ring.lift G f A (g:= fun i => (g i).toRingHom) (Hg:=Hg)) with
+  commutes' := by
+    let i := Classical.arbitrary ι
+    intro r
+    let of := Algebra.of G f i
+    let ring_of := of.toRingHom
+    let algMap_limit := algebraMap R (DirectLimit G f)
+    let algMap_Gi := algebraMap R (G i)
+    let algMap_A := algebraMap R A
+    let lift := DirectLimit.Ring.lift G f A (g:= fun i => (g i).toRingHom) (Hg:=Hg)
+    have lift_of : lift (ring_of (algMap_Gi r)) = (g i).toRingHom (algMap_Gi r) :=
+      DirectLimit.Ring.lift_of A (g:= fun i => (g i).toRingHom) Hg i (algMap_Gi r)
+    calc
+      lift (algMap_limit r) = lift (of (algMap_Gi r)) := by rw [AlgHom.commutes]
+      _ = lift (ring_of (algMap_Gi r)) := by rfl
+      _ = (g i).toRingHom (algMap_Gi r) := lift_of
+      _ = (g i) (algMap_Gi r) := by rfl
+      _ = algMap_A r := by rw [AlgHom.commutes]
+}
+
+/-
+    calc
+      DirectLimit.Ring.lift G f A (g := fun i => (g i).toRingHom) (Hg := Hg)
+          (algebraMap R (DirectLimit G f) r)
+          = DirectLimit.Ring.lift G f A (g := fun i => (g i).toRingHom) (Hg := Hg)
+              ((DirectLimit.Algebra.of G f i) (algebraMap R (G i) r)) := by
+
+
+    have h : (DirectLimit.Ring.lift G f A (g:= fun i => (g i).toRingHom) (Hg:=Hg)).toFun = _root_.DirectLimit.lift _ (g · ·) fun i j h x ↦ (Hg i j h x).symm := rfl
+    let foo := _root_.DirectLimit.lift _ (g · ·) fun i j h x ↦ (Hg i j h x).symm
+    intro r
+    --rw [RingHom.toFun_eq_coe]
+    rw [h]
+    have Hg2 : ∀ i j hij x, (g i).toRingHom x = (g j).toRingHom (f i j hij x) := by
+      intro i j hij x
+      simp [Hg]
+    have h2 := lift_def (F:=G) f (fun i => (g i).toRingHom) Hg2
+    rw [h2]
+    rw [lift_def]
+    simp
+    sorry
+-/
+
 
 
 end Algebra
