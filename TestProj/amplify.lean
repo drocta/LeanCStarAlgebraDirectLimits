9628d195811a46994df4b2f0be3fccbdf872ea10
drocta <madaco.madaco@gmail.com>
2026-04-17 15:47:26 -0700

    amplify.lean thought these changes were already committed, just more versions of amplify_assoc

--- TestProj/amplify.lean
+++ TestProj/amplify.lean
@@ -124,12 +124,79 @@ end StarAlg
 end Matrix
 
 
-lemma amplify_assoc
+lemma amplify_assoc_reindex
   (n k k2 : Type*) [Fintype n] [DecidableEq n] [Fintype k] [DecidableEq k]
   [Fintype k2] [DecidableEq k2] :
-  ((Matrix.reindexStarAlgEquiv 𝕜 𝕜 (Equiv.prodAssoc n k k2)) : _ →⋆ₐ[𝕜] _ ).comp
+  ((Matrix.reindexStarAlgEquiv 𝕜 𝕜 (Equiv.prodAssoc n k k2)) : _ →⋆ₐ[𝕜] _).comp
     ((amplify (n × k) k2).comp (amplify n k)) =
-  (amplify n (k × k2)) := by
+  amplify n (k × k2) := by
   ext A
   have h := Matrix.kronecker_assoc A (1 : Matrix k k 𝕜) (1 : Matrix k2 k2 𝕜)
   simp [amplify_apply, h]
+
+
+lemma amplify_assoc
+  (n k k2 : Type*) [Fintype n] [DecidableEq n] [Fintype k] [DecidableEq k]
+  [Fintype k2] [DecidableEq k2] :
+  ((amplify (n × k) k2).comp (amplify n k)) =
+    ((Matrix.reindexStarAlgEquiv 𝕜 𝕜 (Equiv.prodAssoc n k k2).symm) : _ →⋆ₐ[𝕜] _).comp
+      (amplify n (k × k2)) := by
+    let e := ((Matrix.reindexStarAlgEquiv 𝕜 𝕜 (Equiv.prodAssoc n k k2)))
+    let e₂ : Matrix (n × k × k2) (n × k × k2) 𝕜 →⋆ₐ[𝕜] Matrix ((n × k) × k2) ((n × k) × k2) 𝕜 := e.symm
+    have h := @amplify_assoc_reindex 𝕜 _ n k k2 _ _ _ _ _ _
+    have h1 := congrArg (fun φ => (e₂.comp φ)) h
+    exact h1
+
+
+
+lemma amplify_assoc2
+  (n k k2 : Type*) [Fintype n] [DecidableEq n] [Fintype k] [DecidableEq k]
+  [Fintype k2] [DecidableEq k2] :
+  ((amplify (n × k) k2).comp (amplify n k)) =
+    ((Matrix.reindexStarAlgEquiv 𝕜 𝕜 (Equiv.prodAssoc n k k2).symm) : _ →⋆ₐ[𝕜] _).comp
+      (amplify n (k × k2)) := by
+  have h := @amplify_assoc_reindex 𝕜 _ n k k2 _ _ _ _ _ _
+  simpa using
+    congrArg
+      (fun φ =>
+        (((Matrix.reindexStarAlgEquiv 𝕜 𝕜 (Equiv.prodAssoc n k k2)).symm :
+        _ →⋆ₐ[𝕜] Matrix ((n × k) × k2) ((n × k) × k2) 𝕜).comp φ))
+      h
+
+lemma amplify_assoc3
+  (n k k2 : Type*) [Fintype n] [DecidableEq n] [Fintype k] [DecidableEq k]
+  [Fintype k2] [DecidableEq k2] :
+  ((amplify (n × k) k2).comp (amplify n k)) =
+    ((Matrix.reindexStarAlgEquiv 𝕜 𝕜 (Equiv.prodAssoc n k k2).symm) : _ →⋆ₐ[𝕜] _).comp
+      (amplify n (k × k2)) := by
+  let e :
+      Matrix (n × (k × k2)) (n × (k × k2)) 𝕜 →⋆ₐ[𝕜]
+        Matrix ((n × k) × k2) ((n × k) × k2) 𝕜 :=
+    (Matrix.reindexStarAlgEquiv 𝕜 𝕜 (Equiv.prodAssoc n k k2)).symm
+  have h := @amplify_assoc_reindex 𝕜 _ n k k2 _ _ _ _ _ _
+  simpa using congrArg (fun φ => (e.comp φ)) h
+
+private def StarAlgEquiv.toStarAlgHom {R A B : Type*} [CommSemiring R] [Semiring A] [Semiring B]
+    [Star A] [Star B] [Algebra R A] [Algebra R B]
+    (e : A ≃⋆ₐ[R] B) : A →⋆ₐ[R] B :=
+  (e : A →⋆ₐ[R] B)
+
+
+lemma amplify_assoc4
+  (n k k2 : Type*) [Fintype n] [DecidableEq n] [Fintype k] [DecidableEq k]
+  [Fintype k2] [DecidableEq k2] :
+  ((amplify (n × k) k2).comp (amplify n k)) =
+    ((Matrix.reindexStarAlgEquiv 𝕜 𝕜 (Equiv.prodAssoc n k k2).symm) : _ →⋆ₐ[𝕜] _).comp
+      (amplify n (k × k2)) := by
+  let e := (Matrix.reindexStarAlgEquiv 𝕜 𝕜 (Equiv.prodAssoc n k k2)).symm.toStarAlgHom
+  have h := @amplify_assoc_reindex 𝕜 _ n k k2 _ _ _ _ _ _
+  simpa using congrArg (fun φ => (e.comp φ)) h
+
+lemma amplify_assoc_manualIndexTwiddling
+    (n k k2 : Type*) [Fintype n] [DecidableEq n] [Fintype k] [DecidableEq k]
+    [Fintype k2] [DecidableEq k2] :
+    (amplify (n × k) k2).comp (amplify n k) =
+      ((Matrix.reindexStarAlgEquiv 𝕜 𝕜 (Equiv.prodAssoc n k k2).symm) : _ →⋆ₐ[𝕜] _).comp
+        (amplify n (k × k2)) := by
+  ext A
+  simp [amplify_apply, Matrix.one_apply, ← ite_and, and_comm]
